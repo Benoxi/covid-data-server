a597eb2dcb21a735b0da8acba571b09cd7610dca
Benjamin Bohak <benjamin.bohak@novelus.si>
2020-03-17 22:24:50 +0100

    Inital heroku setup

---
+++ api.py
@@ -0,0 +1,43 @@
+import flask
+from flask import request, jsonify
+from flask_cors import CORS, cross_origin
+import requests
+import schedule
+import time
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+#app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app, resources = {
+        r'/api/*': {
+            "origins": 
+            [
+                "http://localhost:8080",
+                "http://covid-data-client.herokuapp.com"
+            ]
+        }
+    }
+)
+
+# CORS(app, resources=r'/api/*')
+
+global covidData, countryData
+
+# //? TODO: Add simple API call that gets, fresh data!
+
+# //! TODO: Add database
+# //! TODO: Add countries - requests.get("https://restcountries.eu/rest/v2/all")
+# //! TODO: Format countries
+
+def queryForData():
+    covidData = requests.get("https://lab.isaaclin.cn/nCoV/api/area")
+
+schedule.every(10).minutes.do(queryForData)
+
+covidData = requests.get("https://lab.isaaclin.cn/nCoV/api/area")
+
+@app.route('/api/v1/covid', methods=['GET'])
+def covidDataRequest():
+    return jsonify(covidData.text   );
+
+app.run()
